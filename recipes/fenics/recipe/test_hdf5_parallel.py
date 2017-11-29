5ed713f72d848b409f4bbf7cb6a5c9e451f30519
Eric Chen <eric@ericnchen.com>
2017-11-29 14:14:49 +0100

    Fix hdf5 parallel testing script

--- recipes/fenics/recipe/test_hdf5_parallel.py
+++ recipes/fenics/recipe/test_hdf5_parallel.py
@@ -7,21 +7,28 @@ This script tests that HDF5 was compiled to work in parallel.
 
 
 def test_hdf5_parallel():
-    from random import randint
-    suffix = ''.join((str(randint(1, 1000)), str(randint(1, 1000))))
-    filename = '/tmp/mesh{}.hdf5'.format(suffix)
+    """
+    Test that meshes can be exported to HDF5 format in parallel.
+    """
 
     # noinspection PyUnresolvedReferences
     from dolfin import UnitCubeMesh
     mesh = UnitCubeMesh(1, 1, 1)
+    comm = mesh.mpi_comm()
+    name = 'test_mesh.hdf5'
 
     # noinspection PyUnresolvedReferences
     from dolfin import HDF5File
-    with HDF5File(mesh.mpi_comm(), filename, 'w') as f:
+    with HDF5File(comm, name, 'w') as f:
         f.write(mesh, 'mesh')
 
-    from os import remove
-    remove(filename)
+    # Clean up
+
+    # noinspection PyUnresolvedReferences
+    from dolfin import MPI
+    if MPI.rank(comm) == 0:
+        from os import remove
+        remove(name)
 
 
 if __name__ == '__main__':

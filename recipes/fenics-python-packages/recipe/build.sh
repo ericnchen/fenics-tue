d86f9a35bc6d7373ff5aa6fa6a24afe52e5a8b9c
Eric Chen <eric@ericnchen.com>
2017-11-30 18:12:38 +0100

    Finish instant

--- recipes/fenics-python-packages/recipe/build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-(cd dijitso && "${PYTHON}" setup.py install)
-(cd ufl     && "${PYTHON}" setup.py install)
-(cd instant && "${PYTHON}" setup.py install)
-(cd fiat    && "${PYTHON}" setup.py install)
-(cd ffc     && "${PYTHON}" setup.py install)

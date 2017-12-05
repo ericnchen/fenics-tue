3d14c53d9c5b1753f82238acda575dc1630842a7
Eric Chen <eric@ericnchen.com>
2017-12-05 12:59:57 +0100

    Add internet/proxy logic to scripts

--- install_conda.sh
+++ install_conda.sh
@@ -8,6 +8,16 @@ FN="/tmp/miniconda${RANDOM}${RANDOM}.sh"
 URL='https://repo.continuum.io/miniconda/Miniconda3-4.3.30-Linux-x86_64.sh'
 MD5='0b80a152332a4ce5250f3c09589c7a81'
 
+bash check_internet.sh
+if [ "${?}" != "0" ]; then
+  if [ "$(hostname -s)" == "rng" ] || [ "$(hostname -s)" == "furnace" ]; then
+    export https_proxy="https://proxy.wfw.wtb.tue.nl:443"
+  else
+    echo "No internet connectivity available."
+    exit 1
+  fi
+fi
+
 wget -q -O "${FN}" "${URL}"
 if [ "$(md5sum ${FN} | awk '{ print $1 }')" != "${MD5}" ]; then
   echo "The Anaconda installation file downloaded from ${URL} was corrupted."

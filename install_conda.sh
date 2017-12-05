30b40006ebf3a294bc2090dd600c28caaa0d47f5
Eric Chen <eric@ericnchen.com>
2017-12-05 09:05:19 +0100

    Add blank line and extra echo statement to scripts.

--- install_conda.sh
+++ install_conda.sh
@@ -11,6 +11,7 @@ MD5='0b80a152332a4ce5250f3c09589c7a81'
 wget -q -O "${FN}" "${URL}"
 if [ "$(md5sum ${FN} | awk '{ print $1 }')" != "${MD5}" ]; then
   echo "The Anaconda installation file downloaded from ${URL} was corrupted."
+  echo "Try running this script again."
   exit 1
 fi
 

3c183b133f5671f669e3d99284784091a6684bfd
Eric Chen <eric@ericnchen.com>
2017-12-10 17:58:37 +0100

    Add tue feature package and modified mumps to use it.

---
+++ recipes/mumps/run_test.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+cd examples
+
+./ssimpletest < input_simpletest_real
+./dsimpletest < input_simpletest_real
+./csimpletest < input_simpletest_cmplx
+./zsimpletest < input_simpletest_cmplx
+./c_example

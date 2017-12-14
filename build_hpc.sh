5cc03a1175c95b734e7cea2d51c751e792c8743d
Eric Chen <eric@ericnchen.com>
2017-12-14 10:49:46 +0100

    Edit fenics-hpc-components recipe

---
+++ build_hpc.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Build all conda packages in the appropriate order for fenics-hpc.
+# TODO This file is currently just a skeleton/for testing purposes.
+
+#bash conda_build tue  # leave in; might change to tue-hpc?
+
+#bash conda_build petsc
+
+#bash conda_build gts
+
+bash conda_build fenics-hpc-components
+
+#bash conda_build dolfin
+
+#bash conda_build fenics
+
+#bash conda_build purge

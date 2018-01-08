d165e1e538c4d04c8b3d9368205d80508f876ac3
Eric Chen <eric@ericnchen.com>
2018-01-08 16:34:15 +0100

    Move MUMPs test back into build script cause it broke the build and I didn't want to figure it out.

--- recipes/mumps/build.sh
+++ recipes/mumps/build.sh
@@ -6,3 +6,11 @@ make -j 1 all
 
 cp lib/*.a     "${PREFIX}/lib"
 cp include/*.h "${PREFIX}/include"
+
+cd examples
+
+./ssimpletest < input_simpletest_real
+./dsimpletest < input_simpletest_real
+./csimpletest < input_simpletest_cmplx
+./zsimpletest < input_simpletest_cmplx
+./c_example
--- recipes/mumps/run_test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cd examples
-
-./ssimpletest < input_simpletest_real
-./dsimpletest < input_simpletest_real
-./csimpletest < input_simpletest_cmplx
-./zsimpletest < input_simpletest_cmplx
-./c_example

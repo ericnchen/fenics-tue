9f379f256208c1d4e7643a83f0c38081389af36b
Eric Chen <eric@ericnchen.com>
2018-01-16 20:33:05 +0100

    Add/update ScaLAPACK recipe.

--- recipes/scalapack/build.sh
+++ recipes/scalapack/build.sh
@@ -1,8 +1,25 @@
 #!/usr/bin/env bash
-
 set -e
 
-cp "${RECIPE_DIR}/Makefile" Makefile
-cp "${RECIPE_DIR}/SLmake.inc.example" SLmake.inc
-#make -j "${CPU_COUNT}" lib
-env
+# Unset the default compile/link flags that the conda compiler tools set.
+unset \
+  DEBUG_FORTRANFLAGS \
+  CXXFLAGS \
+  DEBUG_CXXFLAGS \
+  DEBUG_FFLAGS \
+  FORTRANFLAGS \
+  CFLAGS \
+  DEBUG_CFLAGS \
+  FFLAGS
+
+ln -s "${GCC}" "${CONDA_PREFIX}/bin/gcc"
+ln -s "${GXX}" "${CONDA_PREFIX}/bin/g++"
+ln -s "${GFORTRAN}" "${CONDA_PREFIX}/bin/gfortran"
+
+cp "${RECIPE_DIR}/SLmake.inc" SLmake.inc
+cp "${RECIPE_DIR}/Makefile.TOOLS" TOOLS/Makefile
+
+make toolslib pblaslib redistlib scalapacklib
+
+mkdir "${PREFIX}/lib"
+cp libscalapack.a "${PREFIX}/lib/libscalapack.a"

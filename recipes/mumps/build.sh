cfc807a0c87a41eb648aa9334e1824d1cd556698
Eric Chen <eric@ericnchen.com>
2018-01-17 14:22:45 +0100

    Update MUMPS.

--- recipes/mumps/build.sh
+++ recipes/mumps/build.sh
@@ -1,16 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -e
 
-cp "${RECIPE_DIR}/Makefile.inc.PAR" ./Makefile.inc
+# Unset the default compile/link flags that the conda compiler tools set.
+unset \
+  DEBUG_FORTRANFLAGS \
+  CXXFLAGS \
+  DEBUG_CXXFLAGS \
+  DEBUG_FFLAGS \
+  FORTRANFLAGS \
+  CFLAGS \
+  DEBUG_CFLAGS \
+  FFLAGS
 
-make -j 1 all
+ln -s "${RECIPE_DIR}/Makefile.inc" Makefile.inc
 
+export AR="${AR} vr "
+
+# I don't think passing the -j NP flag results in successful builds 100%.
+make alllib
+
+# Manually install.
 cp lib/*.a     "${PREFIX}/lib"
 cp include/*.h "${PREFIX}/include"
-
-cd examples
-
-./ssimpletest < input_simpletest_real
-./dsimpletest < input_simpletest_real
-./csimpletest < input_simpletest_cmplx
-./zsimpletest < input_simpletest_cmplx
-./c_example

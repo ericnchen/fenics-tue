aa9cb30914000042d2739800171534b23185fea5
Eric Chen <eric@ericnchen.com>
2018-01-20 13:33:39 +0100

    Add hypre recipe.

---
+++ recipes/hypre/build.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+set -e
+
+# Unset the default compile/link flags that the conda compiler tools set.
+unset \
+  DEBUG_FORTRANFLAGS \
+  CXXFLAGS \
+  DEBUG_CXXFLAGS \
+  DEBUG_FFLAGS \
+  FORTRANFLAGS \
+  CFLAGS \
+  DEBUG_CFLAGS \
+  FFLAGS
+
+cp "${RECIPE_DIR}/Makefile" src/Makefile
+
+cd src
+
+./configure --prefix="${PREFIX}" \
+  CPPFLAGS="-I${PREFIX}/include" \
+  CC=mpicc                       \
+  CXX=mpicxx                     \
+  FC=mpifort                     \
+  CFLAGS="-O3"                   \
+  CXXFLAGS="-O3"                 \
+  FFLAGS="-O3"                   \
+  AR="${AR} -rcu"                \
+  --with-blas-lib="${PREFIX}/lib/libopenblas.so" \
+  --with-lapack-lib="${PREFIX}/lib/liblapack.so" \
+  --with-fei=no
+
+make
+
+make check
+make CHECKRUN="mpirun -np 4" checkpar
+
+make install
